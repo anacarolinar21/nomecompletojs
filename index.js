098485cc3ae73c7b4e823b35690d2affd806d25b
Anacarolinar21 <anacarolinarodriguescarolina2@gmail.com>
2022-11-17 21:30:57 -0300

    exercicio finalizado

---
+++ index.js
@@ -0,0 +1,4 @@
+const Nome = prompt("Digite seu nome:");
+const Sobrenome = prompt("Digite seu sobrenome:");
+
+const NomeCompleto = confirm(`Seu nome completo é ${Nome} ${Sobrenome}\nSeu nome de catalogo é ${Sobrenome.toUpperCase()} ${Nome}`);
